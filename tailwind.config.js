e4da341e37345369fe1cdaf888a493b78a2d3ae4
Azka norouzi <a.azkanorouzi@gmail.com>
2023-12-11 07:45:43 -0500

    style: add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
     content: ["./*.html", "./src/*.tsx","./src/*.jsx", "./src/**/*.tsx", "./src/**/*.jsx","./src/**/**/*.tsx", "./src/**/**/*.js"],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                neutral: "rgb(40, 0, 90)",
+                primary: "rgb(63, 0, 113)",
+                secondary: "rgb(51, 47, 208)",
+                tertiary: "rgb(0, 2, 161)",
+                accent: "rgb(255, 20, 70)",
+            }
+        }
     },
     plugins: [require("daisyui")],
 } 
