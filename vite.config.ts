92c4b926fc56222292ddb7802d2335ed7900dd90
Azka norouzi <a.azkanorouzi@gmail.com>
2023-12-12 09:17:18 -0500

    Add tests to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    global: true,
+    environment: 'jsdom',
+    setUpFiles: './tests/setup.js',
+  },
 })
