03773b0b29d2d9cb65ddfd7381cb027f0a109010
Azka norouzi <a.azkanorouzi@gmail.com>
2023-12-12 09:17:37 -0500

    Add basic testing

---
+++ tests/title.test.tsx
@@ -0,0 +1,27 @@
+import { it, describe, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import Title from '../src/ui/Title'
+
+describe('given', () => {
+    it('renders correct heading 1', () => {
+      render(<Title title="test" headingNumber={1} />)
+      expect(screen.getByRole('heading').textContent).toMatch(/test/i)
+    })
+    it('renders correct heading 2', () => {
+        render(<Title title="test4" headingNumber={4}/>)
+        expect(screen.getByRole('heading', {level: 4}).textContent).toMatch(/test4/i)
+    })
+    it('renders correct heading 3', () => {
+        render(<Title title="test3" headingNumber={3}/>)
+        expect(screen.getByRole('heading', {level: 3}).textContent).toMatch(/test3/i)
+    })
+    it('renders correct heading 5', () => {
+        render(<Title title="test5" headingNumber={5}/>)
+        expect(screen.getByRole('heading', {level: 5}).textContent).toMatch(/test5/i)
+    })
+    it('Handles incorrect heading (0)', () => {
+        render(<Title title="test0" headingNumber={0}/>)
+        expect(screen.getByRole('heading', {level: 6}).textContent).toMatch(/test0/i)
+    })
+})
