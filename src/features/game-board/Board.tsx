b046b8568d1fc581275b8ba1e17898aa691ef5eb
Azka norouzi <a.azkanorouzi@gmail.com>
2023-12-18 15:03:00 -0500

    Add game board

---
+++ src/features/game-board/Board.tsx
@@ -0,0 +1,13 @@
+import Card from '../../ui/Card'
+import Title from '../../ui/Title'
+import Grid from '../../ui/Grid'
+import RadialGradientEffect from '../../ui/RadialGradientEffect'
+
+export default function Board() {
+  return (
+    <Card title={<Title headingNumber={1} title='BATTLE SHIP' type='neon' customStyles='text-accent flex'/>} customStyle=" lg:w-auto w-screen" initial={{scale: 0}} animate={{scale: 1}} transition={{delay:.2}}>
+    <Grid row={10} col={10} customCellStyles="bg-[rgba(0,0,0,0.5)]"/>  
+    <RadialGradientEffect color1="rgba(230,0,0,.0)" color2="rgba(230,0,0,0.2)"/>
+    </Card>
+  )
+}
