cd48417f11c9ca17a47ef62e9558a08fa78f89cb
Azka norouzi <a.azkanorouzi@gmail.com>
2023-12-18 15:07:36 -0500

    Add messages.ts

---
+++ src/data/messages.ts
@@ -0,0 +1,9 @@
+const messages = {
+    initial: 'Welcome! this is a terminal based implementation of battleship game, to start the game you can write the start command, or get more info by clicking on the ? button next to the terminal.',
+    preparation: {
+        initial: "Welcome to the board chief, we're about to start a war! but first we need to prepare the board by positioning our ships, to get our enemy destroyed. you can drag and drop, or select your ships. and put them on the board manually, or you can use this command put <shipName> <position> command",
+        finished: "Alright chief, now we're ready to destroy those bastards, you can click on the confirm button to finish preparation phase, or you can run board confirm command",
+    }
+}
+
+export default messages;
