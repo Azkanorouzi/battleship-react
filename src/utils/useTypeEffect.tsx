0d85c03b210c5f195fad5996b8d5e5d109a1fa3d
Azka norouzi <a.azkanorouzi@gmail.com>
2023-12-18 15:06:44 -0500

    Add type Effect hook

---
+++ src/utils/useTypeEffect.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+interface UseTypeEffectProps {
+    text: string;
+    speed?: number;
+}
+
+export default function useTypeEffect({text, speed = 100} : UseTypeEffectProps) {
+    const [curMessage, setCurMessage] = useState('');
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          if (text[curMessage.length] === undefined) {
+            clearInterval(intervalId)
+            return;
+          }
+          
+          setCurMessage(() => {
+            return curMessage + text[curMessage.length];
+          }) 
+        }, speed)
+        return () => clearInterval(intervalId)
+      }, [text, speed, curMessage])
+      return curMessage;
+}
